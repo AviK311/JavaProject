4a170d03cb64ef6db4794c3902cd9b13c4b98678
תמר גולדשמיד <tgoldshm@g.jct.ac.il>
2019-03-16 20:53:08 +0200

    111

--- untitled/src/geometries/RadialGeometry.java
+++ untitled/src/geometries/RadialGeometry.java
@@ -1,4 +1,20 @@
 package geometries;
 
 public class RadialGeometry {
+    float radius;
+
+    public RadialGeometry(float radius) {
+        this.radius = radius;
+    }
+    public RadialGeometry(RadialGeometry other) {
+        this.radius = other.radius;
+    }
+
+    public float getRadius() {
+        return radius;
+    }
+
+    public void setRadius(float radius) {
+        this.radius = radius;
+    }
 }
