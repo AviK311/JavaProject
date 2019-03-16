8f12c4e06fb61f790d5a9e8fb358c4a73b23633d
Avi Koenigsberg <abkoen@gmail.com>
2019-03-16 23:20:15 +0200

    Ray

---
+++ untitled/src/primitives/Ray.java
@@ -0,0 +1,43 @@
+package primitives;
+
+public class Ray {
+    private Vector direction;
+    private Point3D head;
+
+    public Ray(Vector direction, Point3D head) {
+        this.direction = direction;
+        this.head = head;
+    }
+
+    public Vector getDirection() {
+        return direction;
+    }
+
+    public void setDirection(Vector direction) {
+        this.direction = direction;
+    }
+
+    public Point3D getHead() {
+        return head;
+    }
+
+    public void setHead(Point3D head) {
+        this.head = head;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Ray)) return false;
+        Ray ray = (Ray) o;
+        return getHead().equals(ray.getHead()) && getDirection().equals(ray.getDirection());
+    }
+
+    @Override
+    public String toString() {
+        return "Ray{" +
+                "head=" + head +
+                ", direction=" + direction +
+                '}';
+    }
+}
