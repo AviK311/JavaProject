cd8b1070e41f9bcd4334a5b0348a3f0c972f8d56
תמר גולדשמיד <tgoldshm@g.jct.ac.il>
2019-03-20 00:13:19 +0200

    111

--- untitled/src/Tests/primitives/VectorTest.java
+++ untitled/src/Tests/primitives/VectorTest.java
@@ -6,6 +6,15 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class VectorTest {
+    Coordinate c0=new Coordinate(0);
+    Coordinate c1=new Coordinate(1);
+    Coordinate c2=new Coordinate(2);
+    Coordinate c3=new Coordinate(3);
+    Coordinate c4=new Coordinate(4);
+    Point3D p1=new Point3D(c0,c1,c2);
+    Point3D p2=new Point3D(c1,c2,c2);
+    Vector v1=new Vector(p1);
+    Vector v2=new Vector(p2);
 
     @Before
     public void setUp() throws Exception {
@@ -13,27 +22,16 @@ public class VectorTest {
 
     @Test
     public void add() {
-        Coordinate c1=new Coordinate(1);
-        Coordinate c2=new Coordinate(2);
-        Point3D p1=new Point3D(c1,c1,c1);
-        Point3D p2=new Point3D(c2,c2,c2);
-        Vector v1=new Vector(p1);
-        Vector v2=new Vector(p2);
-        Vector result=v1.add(v1);
-        assertEquals("good",v2,result);
+        Point3D p3=new Point3D(c1,c3,c4);
+        Vector v3= new Vector(p3);
+        Vector result=v1.add(v2);
+        assertEquals(v3,result);
     }
 
     @Test
     public void subtract() {
-        Coordinate c1=new Coordinate(1);
-        Coordinate c2=new Coordinate(2);
-        Coordinate c0=new Coordinate(0);
-        Point3D p1=new Point3D(c0,c1,c2);
-        Point3D p2=new Point3D(c1,c2,c2);
         Point3D p3=new Point3D(c1,c1,c0);
-        Vector v1=new Vector(p1);
-        Vector v2=new Vector(p2);
-        Vector v3=new Vector(p3);
+        Vector v3= new Vector(p3);
         Vector result=v2.subtract(v1);
         assertEquals("good",v3,result);
     }
