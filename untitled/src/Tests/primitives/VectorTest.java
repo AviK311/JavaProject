04235135eaa4e52638e35815c452bbdc384e0ac1
Avi Koenigsberg <abkoen@gmail.com>
2019-03-24 17:22:25 +0200

    some shit

--- untitled/src/Tests/primitives/VectorTest.java
+++ untitled/src/Tests/primitives/VectorTest.java
@@ -14,7 +14,7 @@ public class VectorTest {
     @Test
     public void add() {
         Coordinate c=new Coordinate(1);
-        Coordinate c2=new Coordinate(2);
+        Coordinate c2=new Coordinate(3);
         Point3D p=new Point3D(c,c,c);
         Point3D p2=new Point3D(c2,c2,c2);
         Vector v=new Vector(p);
