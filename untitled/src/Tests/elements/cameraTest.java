217596353c33a5668c767bb1fa32ace1fecd9ee9
תמר גולדשמיד <tgoldshm@g.jct.ac.il>
2019-03-26 13:24:59 +0200

    111

---
+++ untitled/src/Tests/elements/cameraTest.java
@@ -0,0 +1,16 @@
+package elements;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class cameraTest {
+
+    @Test
+    public void getvRight() {
+    }
+
+    @Test
+    public void constructRayThrowAPixel() {
+    }
+}
