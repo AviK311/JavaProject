63df3a75293e96f9c3545680578488ef2ad6ec84
Avi Koenigsberg <abkoen@gmail.com>
2019-03-28 16:37:23 +0200

    TODOs

--- untitled/src/Tests/elements/cameraTest.java
+++ untitled/src/Tests/elements/cameraTest.java
@@ -8,9 +8,11 @@ public class cameraTest {
 
     @Test
     public void getvRight() {
+        // TODO: 3/28/2019 Add a test for getvRight
     }
 
     @Test
     public void constructRayThrowAPixel() {
+        // TODO: 3/28/2019 Add a test for constructRayThrowAPixel
     }
 }
