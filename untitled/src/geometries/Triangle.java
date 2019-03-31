1fe23865306084b15416dd05def519e2dce15946
תמר גולדשמיד <tgoldshm@g.jct.ac.il>
2019-04-01 00:52:45 +0300

    111

--- untitled/src/geometries/Triangle.java
+++ untitled/src/geometries/Triangle.java
@@ -1,8 +1,10 @@
 package geometries;
 
 import primitives.Point3D;
+import primitives.Ray;
 import primitives.Vector;
 
+import java.util.ArrayList;
 import java.util.Objects;
 
 public class Triangle extends Plane{
@@ -27,5 +29,26 @@ public class Triangle extends Plane{
     @Override
     public Vector getNormal(Point3D p) {
        return this.normal;
+    }
+
+    @Override
+    public ArrayList<Point3D> FindIntersections(Ray ray) {
+        ArrayList<Point3D> intersectionPoints = super.FindIntersections(ray);
+        Vector v0= intersectionPoints.get(0).subtract(ray.getHead()); // p - p0
+        Vector v1= p1.subtract(ray.getHead()); // T1 - P0
+        Vector v2= p2.subtract(ray.getHead()); // T2 - P0
+        Vector v3= p3.subtract(ray.getHead()); // T2 - P0
+        Vector v21 = v2.crossProduct(v1); // v2 x v1
+        Vector v13 = v1.crossProduct(v3); // v1 x v3
+        Vector v32 = v3.crossProduct(v2); // v3 x v2
+        Vector  N1 = v21.scale(1.0/v21.length()); // v2 x v1 / |v2 x v1|
+        Vector  N2 = v13.scale(1.0/v13.length()); // v2 x v1 / |v2 x v1|
+        Vector  N3 = v32.scale(1.0/v32.length()); // v2 x v1 / |v2 x v1|
+        double sign1=v0.dotProduct(N1);
+        double sign2=v0.dotProduct(N2);
+        double sign3=v0.dotProduct(N3);
+        if ((sign1>0&&sign2>0&&sign3>0)||(sign1<0&&sign2<0&&sign3<0))
+            return intersectionPoints;
+        return null;
     }
 }
