87d99c8fba8858592397bb081681a31c2bcfd3bc
תמר גולדשמיד <tgoldshm@g.jct.ac.il>
2019-04-17 01:53:29 +0300

    111

--- untitled/src/Tests/geometries/TriangleTest.java
+++ untitled/src/Tests/geometries/TriangleTest.java
@@ -3,7 +3,10 @@ package geometries;
 import org.junit.Test;
 import primitives.Coordinate;
 import primitives.Point3D;
+import primitives.Ray;
 import primitives.Vector;
+
+import java.util.ArrayList;
 
 import static org.junit.Assert.*;
 
@@ -17,5 +20,21 @@ public class TriangleTest {
         Point3D p3 = new Point3D(zero, one, zero);
         Triangle tri = new Triangle(p1, p2, p3);
         assertEquals(new Vector(new Point3D(zero, zero, one)),tri.getNormal(p1));
+    }
+    @Test
+    public void findIntersections() {
+        Point3D p0 = new Point3D(0,0,0);
+        Point3D p1 = new Point3D(0,100,-200);
+        Point3D p2 = new Point3D(100,-100,-200);
+        Point3D p3 = new Point3D(-100,-100,-200);
+        Triangle triangle=new Triangle(p1,p2,p3);
+        Vector v1 = new Vector(0,0,-1);
+        Ray r =new Ray(v1,p0);
+        ArrayList<Point3D> result = triangle.FindIntersections(r);
+        ArrayList<Point3D> a=new ArrayList<Point3D>();
+        Point3D p4 =new Point3D(0,0,-200);
+        a.add(p4);
+        assertEquals("fail",a,result);
+
     }
 }
