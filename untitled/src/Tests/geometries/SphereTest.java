64a4388a1042e41522750e26f2ca88d51872eb63
תמר גולדשמיד <tgoldshm@g.jct.ac.il>
2019-04-21 22:49:21 +0300

    111

--- untitled/src/Tests/geometries/SphereTest.java
+++ untitled/src/Tests/geometries/SphereTest.java
@@ -3,7 +3,10 @@ package geometries;
 import org.junit.Test;
 import primitives.Coordinate;
 import primitives.Point3D;
+import primitives.Ray;
 import primitives.Vector;
+
+import java.util.ArrayList;
 
 import static org.junit.Assert.*;
 
@@ -17,5 +20,20 @@ public class SphereTest {
         Sphere sp = new Sphere(1.0f, p1);
         assertEquals(new Vector(p2),sp.getNormal(p2));
 
+    }
+
+    @Test
+    public void findIntersections() {
+        Point3D p0 = new Point3D(0,0,0);
+        Sphere sphere = new Sphere(200,new Point3D(0,0,-400));
+        Vector v1 = new Vector(0,0,-1);
+        Ray r =new Ray(v1,p0);
+        ArrayList<Point3D> result = sphere.FindIntersections(r);
+        ArrayList<Point3D> a=new ArrayList<Point3D>();
+        Point3D p1 =new Point3D(0,0,-600);
+        Point3D p2 =new Point3D(0,0,-200);
+        a.add(p1);
+        a.add(p2);
+        assertEquals("fail",a,result);
     }
 }
